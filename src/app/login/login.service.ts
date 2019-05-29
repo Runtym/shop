da655832a44a8512871cd862fc31dab3aeef6d4d
Runtime <redfilaruntime@gmail.com>
2019-05-29 17:06:40 +0900

    add http headers

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,29 +1,32 @@
 import { Injectable } from '@angular/core';
 import { User } from '../user';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
   private base_url:string='http://localhost:88';
-  constructor(private _http:HttpClient) {}
+  private headers:HttpHeaders;
+  constructor(private _http:HttpClient) {
+    
+    var uiId = localStorage
+    .getItem('uiId');
+    var tokken = localStorage
+    .getItem('tokken');
+    this.headers = new  HttpHeaders().set('x-auth-id',uiId).set('x-auth-tokken',tokken);
+  }
   
   login(user:User){
     return this._http.post(this.base_url+'/login',user);
   }
 
   findList(){
-    var uiId = localStorage
-    .getItem('uiId');
-    var tokken = localStorage
-    .getItem('tokken');
-    var param =  '?uiId=' + uiId + 
-    '&tokken=' + tokken;
-    console.log(uiId);
-    console.log(tokken);
+    const headers = this.headers;
     return this._http
-    .get(this.base_url+'/userinfos' + param);
+    .get(this.base_url+'/userinfos',{headers});
   }
   signin(user:User){
     return this._http.post(this.base_url+'/signin',user);
